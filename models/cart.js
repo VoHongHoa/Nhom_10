a97e61beb4cb9551ed293bb5bb44b893624b425f
Tran Van Thien <tranvanthienuit@gmail.com>
2021-11-30 08:14:26 +0700

    file

--- models/cart.js
+++ models/cart.js
@@ -23,9 +23,9 @@ module.exports = function Cart(oldCart) {
 
     this.reduceByOne = function (id) {
         this.items[id].qty--;
-        this.items[id].price -= this.items[id].item.price;
+        this.items[id].price -= this.items[id].item.gia_moi;
         this.totalQty--;
-        this.totalPrice -= this.items[id].item.price;
+        this.totalPrice -= this.items[id].item.gia_moi;
 
         if(this.items[id].qty <= 0) {
             delete this.items[id];
