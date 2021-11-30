40ba2e1a079a48af9374f6a1db0a8b9996806a76
Le Duc Thang <19522197@gm.uit.edu.vn>
2021-11-30 21:32:18 +0700

    file

--- routes/web/index_routers.js
+++ routes/web/index_routers.js
@@ -104,122 +104,6 @@ router.get('/KM/:giam_gia_sp', async function(req, res){
         empty: list.length === 0,
     });
 })
-/*tim san pham*/
-router.get('/ao_polo', async function(req, res){
-
-    const list = await san_phamModel.findloaisp('ao polo');
-    res.render(`web/index`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-
-/*tim san pham trong dropdown*/
-/*ao*/
-router.get('/ao_phong', async function(req, res){
-
-    const list = await san_phamModel.findloaisp('ao phong');
-    res.render(`web/index`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-router.get('/ao_somi', async function(req, res){
-
-    const list = await san_phamModel.findloaisp('ao so mi');
-    res.render(`web/index`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-router.get('/ao_len', async function(req, res){
-
-    const list = await san_phamModel.findloaisp('ao polo');
-    res.render(`web/index`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-/*quan*/
-router.get('/quan_jean', async function(req, res){
-
-    const list = await san_phamModel.findloaisp('quan jean');
-    res.render(`web/index`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-router.get('/quan_au', async function(req, res){
-
-    const list = await san_phamModel.findloaisp('quan au');
-    res.render(`web/index`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-router.get('/quan_kali', async function(req, res){
-
-    const list = await san_phamModel.findloaisp('quan kali');
-    res.render(`web/index`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-router.get('/quan_short', async function(req, res){
-
-    const list = await san_phamModel.findloaisp('quan short');
-    res.render(`web/index`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-/*phu kien nam*/
-router.get('/phu_kien', async function(req, res){
-
-    const list = await san_phamModel.findloaisp('phu kien');
-    res.render(`web/index`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-/* giay dep nam */
-router.get('/giay_dep', async function(req, res){
-
-    const list = await san_phamModel.findloaisp('giay dep');
-    res.render(`web/index`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-
-/* san pham khuyen mai*/
-/* tim san pham khuyen mai 30% trong dropdown*/
-router.get('/khuyen_mai_30', async function(req, res){
-
-    const list = await san_phamModel.findspkm(30);
-    res.render(`web/KM`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-/* tim san pham khuyen mai 30% trong dropdown*/
-router.get('/khuyen_mai_40', async function(req, res){
-
-    const list = await san_phamModel.findspkm(40);
-    res.render(`web/KM`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
-/* tim san pham khuyen mai 30% trong dropdown*/
-router.get('/khuyen_mai_50', async function(req, res){
-
-    const list = await san_phamModel.findspkm(50);
-    res.render(`web/KM`,{
-        san_pham: list,
-        empty: list.length === 0
-    });
-})
 
 /* tin tuc */
 router.get('/tin_tuc',async function (req,res){
@@ -260,8 +144,6 @@ router.get('/search', async  function (req,res){
 router.get('/dangnhap',async function (req,res){
    res.render(`web/dangnhap`, {layout: false})
 })
-
-
 
 
 /* profile*/
